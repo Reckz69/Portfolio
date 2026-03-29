e06994651727f03f603bd033aab25184a28076d8
Reckz69 <narendrameshram182@gmail.com>
2026-03-29 12:45:31 +0530

    vite fixing the html return

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",   // ✅ VERY IMPORTANT
+  base: '/', // This ensures assets are linked relative to the root
 })
